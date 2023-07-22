339bd11c0681cff34ab3a229bd7b0952b58fa815
someaddons <someaddons@github.com>
2023-07-22 22:24:23 +0200

    add clientside config reload

--- src/main/java/com/cupboard/CupboardClient.java
+++ src/main/java/com/cupboard/CupboardClient.java
@@ -1,12 +1,21 @@
 package com.cupboard;
 
+import com.cupboard.config.CupboardConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Minecraft;
 
-public class CupboardClient implements ClientModInitializer
-{
+import static net.minecraft.world.level.Level.OVERWORLD;
+
+
+public class CupboardClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ClientTickEvents.END_WORLD_TICK.register(world -> {
+            if (!Minecraft.getInstance().hasSingleplayerServer() && world.dimension().equals(OVERWORLD)) {
+                CupboardConfig.pollConfigs();
+            }
+        });
     }
 }
