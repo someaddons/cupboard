95585d2afadf892dba9e7d927d4ee7756e2ac7f0
someaddons <someaddons@github.com>
2023-08-10 12:33:09 +0200

    1.18.2

--- src/main/java/com/cupboard/event/ClientEventHandler.java
+++ src/main/java/com/cupboard/event/ClientEventHandler.java
@@ -4,6 +4,7 @@ import com.cupboard.config.CupboardConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,9 +21,9 @@ public class ClientEventHandler
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onClientTick(ScreenEvent.Opening event)
+    public static void onClientTick(ScreenOpenEvent event)
     {
-        if (event.getNewScreen() instanceof TitleScreen)
+        if (event.getScreen() instanceof TitleScreen)
         {
             CupboardConfig.initloadAll();
         }
