c1fb2daa51e2bb397f1ff2e82a424b51bbf105c7
someaddons <someaddons@github.com>
2023-12-24 00:17:08 +0100

    2.2: Add config option to create heap dumps on out of memory errors Improved config updates, additional values no longer reset a config file

--- src/main/java/com/cupboard/Cupboard.java
+++ src/main/java/com/cupboard/Cupboard.java
@@ -2,12 +2,15 @@ package com.cupboard;
 
 import com.cupboard.config.CommonConfiguration;
 import com.cupboard.config.CupboardConfig;
+import com.sun.management.HotSpotDiagnosticMXBean;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.management.ManagementFactory;
 import java.util.Random;
 
 public class Cupboard implements ModInitializer {
@@ -18,7 +21,24 @@ public class Cupboard implements ModInitializer {
     public static Random rand = new Random();
 
     public Cupboard() {
-        //getConfig();
+
+        if (Cupboard.config.getCommonConfig().forceHeapDumpOnOOM)
+        {
+            try
+            {
+                HotSpotDiagnosticMXBean bean = ManagementFactory.newPlatformMXBeanProxy(
+                  ManagementFactory.getPlatformMBeanServer(),
+                  "com.sun.management:type=HotSpotDiagnostic",
+                  HotSpotDiagnosticMXBean.class);
+
+                bean.setVMOption("HeapDumpOnOutOfMemoryError", "true");
+                bean.setVMOption("HeapDumpPath", FabricLoader.getInstance().getGameDir().toString());
+            }
+            catch (Exception e)
+            {
+                LOGGER.error("Failed to enable heapdump option: ", e);
+            }
+        }
 
         ServerTickEvents.END_SERVER_TICK.register(s -> CupboardConfig.pollConfigs());
         ServerLifecycleEvents.SERVER_STARTED.register(s -> CupboardConfig.initloadAll());
--- src/main/java/com/cupboard/config/CommonConfiguration.java
+++ src/main/java/com/cupboard/config/CommonConfiguration.java
@@ -5,8 +5,9 @@ import com.google.gson.JsonObject;
 public class CommonConfiguration implements ICommonConfig
 {
     public boolean showCommandExecutionErrors = true;
-    public boolean debugChunkloadAttempts = false;
-    public boolean logOffthreadEntityAdd = true;
+    public boolean debugChunkloadAttempts     = false;
+    public boolean logOffthreadEntityAdd      = true;
+    public boolean forceHeapDumpOnOOM         = false;
 
     public CommonConfiguration()
     {
@@ -22,14 +23,22 @@ public class CommonConfiguration implements ICommonConfig
         root.add("showCommandExecutionErrors", entry);
 
         final JsonObject entry2 = new JsonObject();
-        entry2.addProperty("desc:", "Enables debug logging of chunks being forceloaded on serverthread by directly accessing an unloaded chunk, which stalls the server until the chunk finishes loading: default:false");
+        entry2.addProperty("desc:",
+          "Enables debug logging of chunks being forceloaded on serverthread by directly accessing an unloaded chunk, which stalls the server until the chunk finishes loading: default:false");
         entry2.addProperty("debugChunkloadAttempts", debugChunkloadAttempts);
         root.add("debugChunkloadAttempts", entry2);
 
         final JsonObject entry3 = new JsonObject();
-        entry3.addProperty("desc:", "Entities should only be added on the server thread itself, cupboard fixes the crashes caused by mods violating that, this option enables the logging of those: default:true");
+        entry3.addProperty("desc:",
+          "Entities should only be added on the server thread itself, cupboard fixes the crashes caused by mods violating that, this option enables the logging of those: default:true");
         entry3.addProperty("logOffthreadEntityAdd", logOffthreadEntityAdd);
         root.add("logOffthreadEntityAdd", entry3);
+
+        final JsonObject entry4 = new JsonObject();
+        entry4.addProperty("desc:",
+          "Enables creating a heap dump automatically once the game crashes with an out of memory issue, use with care heapdumps take a lot of space. default:false");
+        entry4.addProperty("forceHeapDumpOnOOM", forceHeapDumpOnOOM);
+        root.add("forceHeapDumpOnOOM", entry4);
 
         return root;
     }
@@ -39,5 +48,6 @@ public class CommonConfiguration implements ICommonConfig
         showCommandExecutionErrors = data.get("showCommandExecutionErrors").getAsJsonObject().get("showCommandExecutionErrors").getAsBoolean();
         debugChunkloadAttempts = data.get("debugChunkloadAttempts").getAsJsonObject().get("debugChunkloadAttempts").getAsBoolean();
         logOffthreadEntityAdd = data.get("logOffthreadEntityAdd").getAsJsonObject().get("logOffthreadEntityAdd").getAsBoolean();
+        forceHeapDumpOnOOM = data.get("forceHeapDumpOnOOM").getAsJsonObject().get("forceHeapDumpOnOOM").getAsBoolean();
     }
 }
