92b225163a1dddd463dc51328f6544efa3b41f5b
someaddons <someaddons@github.com>
2023-12-24 00:17:08 +0100

    2.3: Add config option to create heap dumps on out of memory errors Improved config updates, additional values no longer reset a config file Fix block search feature not searching down as intended

--- src/main/java/com/cupboard/Cupboard.java
+++ src/main/java/com/cupboard/Cupboard.java
@@ -3,15 +3,18 @@ package com.cupboard;
 import com.cupboard.config.CommonConfiguration;
 import com.cupboard.config.CupboardConfig;
 import com.cupboard.event.EventHandler;
+import com.sun.management.HotSpotDiagnosticMXBean;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.management.ManagementFactory;
 import java.util.Random;
 
 import static com.cupboard.Cupboard.MOD_ID;
@@ -26,6 +29,24 @@ public class Cupboard
 
     public Cupboard()
     {
+        if (Cupboard.config.getCommonConfig().forceHeapDumpOnOOM)
+        {
+            try
+            {
+                HotSpotDiagnosticMXBean bean = ManagementFactory.newPlatformMXBeanProxy(
+                  ManagementFactory.getPlatformMBeanServer(),
+                  "com.sun.management:type=HotSpotDiagnostic",
+                  HotSpotDiagnosticMXBean.class);
+
+                bean.setVMOption("HeapDumpOnOutOfMemoryError", "true");
+                bean.setVMOption("HeapDumpPath", FMLPaths.GAMEDIR.get().toString());
+            }
+            catch (Exception e)
+            {
+                LOGGER.error("Failed to enable heapdump option: ", e);
+            }
+        }
+
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (a, b) -> true));
         Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(EventHandler.class);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
